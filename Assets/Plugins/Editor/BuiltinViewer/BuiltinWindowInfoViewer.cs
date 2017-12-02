e8e2ea9adba7e32d942f3092bb50a2a5e03d0848
wataru-ito <true_black@msn.com>
2017-12-02 15:52:54 +0900

    ウィンドウ解析：プロパティも見れるように

--- Assets/Plugins/Editor/BuiltinViewer/BuiltinWindowInfoViewer.cs
+++ Assets/Plugins/Editor/BuiltinViewer/BuiltinWindowInfoViewer.cs
@@ -10,6 +10,9 @@ namespace BuiltinViewer
 		BindingFlags m_bindingFlags;
 		Vector2 m_scrollPosition;
 
+		readonly string[] kTab = { "Method", "Property" };
+		int m_iTab;
+
 
 		//------------------------------------------------------
 		// static function
@@ -35,53 +38,12 @@ namespace BuiltinViewer
 		{
 			if (!m_window)
 			{
-				EditorGUILayout.LabelField("Window not selected.");
+				EditorGUILayout.HelpBox("Window not selected.", MessageType.Info);
 				return;
 			}
 
-			EditorGUIUtility.labelWidth = 80f;
-
-			EditorGUILayout.LabelField("Window Info");
-			++EditorGUI.indentLevel;
-			{
-				EditorGUILayout.LabelField("titleContent", m_window.titleContent.text);
-				EditorGUILayout.RectField("position", m_window.position);
-				EditorGUILayout.Vector2Field("minSize", m_window.minSize);
-				EditorGUILayout.Vector2Field("maxSize", m_window.maxSize);
-			}
-			--EditorGUI.indentLevel;
-
-
-			EditorGUILayout.LabelField("Class Info");
-			++EditorGUI.indentLevel;
-			{
-				var type = m_window.GetType();
-				EditorGUILayout.LabelField(type.FullName);
-
-				EditorGUILayout.LabelField("Assembly", type.Assembly.ToString());
-				foreach (var attr in type.GetCustomAttributes(true))
-				{
-					EditorGUILayout.LabelField("Attribute", attr.ToString());
-				}
-
-				m_bindingFlags = (BindingFlags)EditorGUILayout.EnumMaskField("Methods", m_bindingFlags);
-				using (var scroll = new EditorGUILayout.ScrollViewScope(m_scrollPosition, "box"))
-				{
-					foreach (var method in type.GetMethods(m_bindingFlags))
-					{
-						var sb = new System.Text.StringBuilder();
-						foreach (var param in method.GetParameters())
-						{
-							if (sb.Length > 0) sb.Append(", ");
-							sb.AppendFormat("{0} {1}", param.ParameterType.Name, param.Name);
-						}
-						EditorGUILayout.LabelField(string.Format("{0} {1} ({2})", method.ReturnType.Name, method.Name, sb.ToString()));
-					}
-
-					m_scrollPosition = scroll.scrollPosition;
-				}
-			}
-			--EditorGUI.indentLevel;
+			DrawWindowInfo();
+			DrawClassInfo();
 		}
 
 		void OnInspectorUpdate()
@@ -92,7 +54,93 @@ namespace BuiltinViewer
 				m_window = focusedWindow;
 			}
 			
-			Repaint();
+			Repaint(); // サイズ等の値をリアルタイム反映させるために毎フレーム描画
+		}
+
+
+		//------------------------------------------------------
+		// window info
+		//------------------------------------------------------
+
+		void DrawWindowInfo()
+		{
+			EditorGUILayout.LabelField("Window Info");
+			++EditorGUI.indentLevel;
+
+			EditorGUIUtility.wideMode = true;
+			
+			EditorGUILayout.LabelField("titleContent", m_window.titleContent.text);
+			EditorGUILayout.RectField("position", m_window.position);
+			EditorGUILayout.Vector2Field("minSize", m_window.minSize);
+			EditorGUILayout.Vector2Field("maxSize", m_window.maxSize);
+		
+			--EditorGUI.indentLevel;
+		}
+
+
+		//------------------------------------------------------
+		// class info
+		//------------------------------------------------------
+
+		void DrawClassInfo()
+		{
+			EditorGUILayout.LabelField("Class Info");
+			++EditorGUI.indentLevel;
+			
+			var type = m_window.GetType();
+			EditorGUILayout.LabelField(type.FullName);
+
+			EditorGUILayout.LabelField("Assembly", type.Assembly.ToString());
+			foreach (var attr in type.GetCustomAttributes(true))
+			{
+				EditorGUILayout.LabelField("Attribute", attr.ToString());
+			}
+
+
+			m_iTab = GUILayout.Toolbar(m_iTab, kTab);
+			m_bindingFlags = (BindingFlags)EditorGUILayout.EnumMaskField("BindingFlags", m_bindingFlags);
+			using (var scroll = new EditorGUILayout.ScrollViewScope(m_scrollPosition, GUI.skin.box))
+			{
+				switch (kTab[m_iTab])
+				{
+					case "Method":
+						DrawMethods(type); 
+						break;
+
+					case "Property":
+						DrawProperties(type);
+						break;
+
+					default:
+						EditorGUILayout.LabelField("Error");
+						break;
+				}
+				m_scrollPosition = scroll.scrollPosition;
+			}
+			
+			--EditorGUI.indentLevel;
+		}
+
+		void DrawMethods(System.Type type)
+		{
+			foreach (var method in type.GetMethods(m_bindingFlags))
+			{
+				var sb = new System.Text.StringBuilder();
+				foreach (var param in method.GetParameters())
+				{
+					if (sb.Length > 0) sb.Append(", ");
+					sb.AppendFormat("{0} {1}", param.ParameterType.Name, param.Name);
+				}
+				EditorGUILayout.LabelField(string.Format("{0} {1} ({2})", method.ReturnType.Name, method.Name, sb.ToString()));
+			}
+		}
+
+		void DrawProperties(System.Type type)
+		{
+			foreach (var property in type.GetProperties(m_bindingFlags))
+			{
+				EditorGUILayout.LabelField(string.Format("{0} {1}", property.PropertyType.Name, property.Name));
+			}
 		}
 	}
 }
